f366b60dafe2325d991d8ddda0be604ab61f17d2
佐野創一 <soichisano@sanosouichinomacbook-ea.local>
2018-08-11 16:51:12 +0900

    secondkadaiapp

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 佐野創一 on 2018/08/10.
+//  Copyright © 2018年 佐野創一. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var name:String = "aaa"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let result = name
+        label.text = "こんにちは、\(result)さん"
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -19,6 +19,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var inputname: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.name = inputname.text!
+
+    }
+    
+
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
 
 
 }
